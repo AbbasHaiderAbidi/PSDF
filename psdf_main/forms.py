31cdfef57c7b49d389f16e476c206e7d05b95b64
AbbasHaiderAbidi <abbashaider2131995@gmail.com>
2021-12-22 12:34:20 +0530

    PROJECT done

--- psdf_main/forms.py
+++ psdf_main/forms.py
@@ -13,10 +13,10 @@ class UserForm(forms.ModelForm):
             'password' : forms.PasswordInput(attrs={'class': 'form-control col-md-6 mb-3 text-center', 'placeholder' : 'Enter password', 'id' : 'password', 'name' : 'password'}),
             'nodal' : forms.TextInput(attrs={'placeholder':'Nodal Officer','class':'form-control col-md-5 mb-3 text-center'}),
             'region' : forms.Select(attrs={'placeholder':'Region','class':'form-control col-md-3 text-center','style':'height:100%;'}, choices=REGIONS),
-            'utilname' : forms.TextInput(attrs={'placeholder':'Name of Utility','class':'form-control col-md-6 mb-3 text-center', 'id':'utility'}),
+            'utilname' : forms.TextInput(attrs={'placeholder':'Name of Utility','class':'form-control col-md-12 mb-3 text-center', 'id':'utility'}),
             'contact' : forms.TextInput(attrs={'placeholder':'Contact Number','class':'form-control col-md-6 mb-3 text-center'}),
             'address' : forms.TextInput(attrs={'id':'address', 'class':'form-control text-center mb-3', 'placeholder':'Enter the Address','style':'height:100%;'}),
-            'email' : forms.EmailInput(attrs={'id':'email', 'class':'form-control text-center mb-3 col-md-5', 'placeholder':'Enter e-mail','style':'height:100%;'})
+            'email' : forms.EmailInput(attrs={'id':'email', 'class':'form-control text-center mb-3 col-md-8', 'placeholder':'Enter e-mail','style':'height:100%;'})
         }
 
 
